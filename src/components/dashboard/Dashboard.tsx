369ea7962d4cdcf2944f7779c9debd6a45cab05b
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-04-16 23:40:05 +0900

    背景画像とコンポーネントを配置

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,5 +1,16 @@
+import { Box } from '@chakra-ui/react';
+import { AddTaskCard } from '../task/AddTaskCard';
+import { TaskCard } from '../task/TaskCard';
+
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Box
+      bgImage="url('mountain.jpeg')"
+      style={{ height: 'calc(100vh - 40px)', backgroundSize: 'cover' }}
+    >
+      Dashboard
+      <TaskCard></TaskCard>
+      <AddTaskCard></AddTaskCard>
+    </Box>
+  );
+};
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { Center, Heading } from '@chakra-ui/react';
 
 export const Header = () => {
   return (
-    <Center as="header" bg={'orange.400'} py={1}>
+    <Center as="header" bg={'orange.400'} h={10} >
       <Heading fontSize="2xl" color="orange.100">
         Trello Clone
       </Heading>
